5e469568010be070c6ba2e980e37ee3596299e89
Soudaki Zakaria <zakaria.soudaki@yahoo.ca>
2024-02-26 09:29:30 -0500

    début de la création des fenêtres de l'application et création de ZoneAnimationPhysique pour les test

--- SpaceSiege/src/application/AppPrincipal14.java
+++ SpaceSiege/src/application/AppPrincipal14.java
@@ -5,6 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AppPrincipal14 extends JFrame {
 
@@ -31,11 +41,57 @@ public class AppPrincipal14 extends JFrame {
 	 */
 	public AppPrincipal14() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1200, 750);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Auteurs");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("New menu item");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenu mnNewMenu_1 = new JMenu("À propos");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
+		mnNewMenu_1.add(mntmNewMenuItem);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 0, 0));
+		panel.setBounds(408, 236, 313, 317);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("SPACE SIEGE");
+		lblNewLabel.setBounds(10, 21, 293, 67);
+		panel.add(lblNewLabel);
+		lblNewLabel.setBackground(new Color(0, 0, 0));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 37));
+		
+		JButton btnNewButton_1 = new JButton("JOUER");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				FenetreDeJeu page = new FenetreDeJeu();
+				dispose();
+			}
+		});
+		btnNewButton_1.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 23));
+		btnNewButton_1.setBounds(46, 105, 216, 69);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("QUITTER");
+		btnNewButton.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 23));
+		btnNewButton.setBounds(46, 196, 216, 69);
+		panel.add(btnNewButton);
 	}
-
 }
